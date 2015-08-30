07bf7499394cf197afbba71137da88c8c5198d14
cuipengfei <cuipengfei2008@gmail.com>
2015-08-30 21:28:52 +0800

    first unit test running

---
+++ .net/NunitCoverage/HowToGetCoverage/HowToGetCoverage/NumAdder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowToGetCoverage
+{
+    public class NumAdder
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ .net/NunitCoverage/HowToGetCoverage/HowToGetCoverageTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+using HowToGetCoverage;
+
+namespace HowToGetCoverageTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void shouldAddOneAndOne()
+        {
+            var adder = new NumAdder();
+            var res = adder.Add(1, 1);
+            Assert.AreEqual(res, 2);
+        }
+    }
+}
