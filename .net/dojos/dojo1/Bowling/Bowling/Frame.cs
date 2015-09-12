0f44d354253e9c03a609de78a7a7e15e17c415b3
cuipengfei <cuipengfei2008@gmail.com>
2015-09-13 00:54:36 +0800

    每一个格子应该可以计算自己的得分

---
+++ .net/dojos/dojo1/Bowling/Bowling/Frame.cs
@@ -0,0 +1,10 @@
+﻿namespace Bowling
+{
+    public class Frame
+    {
+        public int FirstTry { get; set; }
+        public int SecondTry { get; set; }
+
+        public int Score => FirstTry + SecondTry;
+    }
+}
---
+++ .net/dojos/dojo1/Bowling/BowlingTest/FrameTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Bowling;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingTest
+{
+    [TestClass]
+    public class FrameTest
+    {
+        [TestMethod]
+        public void FrameShouldCountItsOwnScore()
+        {
+            //given
+            Frame frame=new Frame() {FirstTry=3,SecondTry=4};
+
+            //when
+            int score = frame.Score;
+
+            //then
+            Assert.AreEqual(7,score);
+        }
+    }
+}
