6431ed4bf265674c076700248379b687a083554b
cuipengfei <cuipengfei2008@gmail.com>
2015-09-13 14:02:16 +0800

    integration done in score board

---
+++ .net/dojos/dojo1/SecondPractice/BowlingSecondPractice/ScoreBoard.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BowlingSecondPractice
+{
+    public class ScoreBoard
+    {
+        private readonly List<Frame> frames=new List<Frame>();
+
+        public int TotalScore
+        {
+            get { return frames.Sum(frame=>frame.Score); }
+        }
+
+        public void PlayFrame(int firstBall, int secondBall, int thirdBall=-1)
+        {
+            Frame frame = null;
+            if (thirdBall != -1)
+            {
+                frame=new LastFrame(firstBall, secondBall, thirdBall);
+            }
+            else
+            {
+                frame=new Frame(firstBall,secondBall);
+            }
+            if (frames.Count > 0)
+            {
+                frames.Last().NextFrame = frame;
+            }
+            frames.Add(frame);
+        }
+    }
+}
---
+++ .net/dojos/dojo1/SecondPractice/BowlingTest/ScoreBoardTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingSecondPractice;
+
+namespace BowlingTest
+{
+    [TestClass]
+    public class ScoreBoardTest
+    {
+        [TestMethod]
+        public void ScoreBoardShouldCalculateTotalScoreOfTenFrames()
+        {
+            //given
+            ScoreBoard scoreBoard=new ScoreBoard();
+
+            //when
+            scoreBoard.PlayFrame(3,4);//7
+            scoreBoard.PlayFrame(2,4);//6
+            scoreBoard.PlayFrame(5,4);//9
+            scoreBoard.PlayFrame(6,4);//20
+            scoreBoard.PlayFrame(10,0);//18
+            scoreBoard.PlayFrame(3,5);//8
+            scoreBoard.PlayFrame(3,7);//13
+            scoreBoard.PlayFrame(3,5);//8
+            scoreBoard.PlayFrame(2,7);//9
+            scoreBoard.PlayFrame(2,8,3);//13
+
+            int totalScore=scoreBoard.TotalScore;
+            
+            //then
+            Assert.AreEqual(111,totalScore);
+        }
+    }
+}
