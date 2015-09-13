00513de45c0c7152de3e2d9ae4a99d02795af36e
cuipengfei <cuipengfei2008@gmail.com>
2015-09-13 13:36:17 +0800

    handle situation when there is no bonus

---
+++ .net/dojos/dojo1/SecondPractice/BowlingSecondPractice/Frame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingSecondPractice
+{
+    public class Frame
+    {
+        public int FirstBall { get; }
+        public int SecondBall { get; }
+
+        public Frame(int firstBall, int secondBall)
+        {
+            this.FirstBall = firstBall;
+            this.SecondBall = secondBall;
+        }
+
+        public int Score { get { return FirstBall + SecondBall; } }
+    }
+}
---
+++ .net/dojos/dojo1/SecondPractice/BowlingTest/FrameTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingSecondPractice;
+
+namespace BowlingTest
+{
+    [TestClass]
+    public class FrameTest
+    {
+        [TestMethod]
+        public void FrameShouldCountScoreOfItsOwn()
+        {
+            //given
+            var frame=new Frame(3,2);
+
+            //when
+            int score=frame.Score;
+
+            //then
+            Assert.AreEqual(5,score);
+        }
+    }
+}
