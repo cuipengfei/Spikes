80df33861a2300539787d16bbf307a548786ebd4
cuipengfei <cuipengfei2008@gmail.com>
2015-09-14 23:44:06 +0800

    total score of normal game

---
+++ .net/dojos/dojo1/ThirdTry/ThirdTry/ScoreBoard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirdTry
+{
+    public class ScoreBoard
+    {
+       private List<Frame> frames=new List<Frame>(); 
+
+        public int TotalScore { get { return frames.Sum(f => f.Score); } }
+
+        public void Play(int firstBall, int secondBall)
+        {
+            var frame=new Frame(firstBall, secondBall);
+            if (frames.Count > 0)
+            {
+                frames.Last().Next = frame;
+            }
+            frames.Add(frame);
+        }
+    }
+}
---
+++ .net/dojos/dojo1/ThirdTry/ThirdTryTest/ScoreBoardTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThirdTry;
+
+namespace ThirdTryTest
+{
+    [TestClass]
+    public class ScoreBoardTest
+    {
+        [TestMethod]
+        public void ScoreBoardShouldCountScoreOfNormalGame()
+        {
+            //given
+            var scoreBoard=new ScoreBoard();
+
+            //when
+            scoreBoard.Play(1,2);//3
+            scoreBoard.Play(2,8);//20
+            scoreBoard.Play(10,0);//19
+            scoreBoard.Play(4,5);//9
+            scoreBoard.Play(7,3);//11
+            scoreBoard.Play(1,2);//3
+            scoreBoard.Play(10,0);//13
+            scoreBoard.Play(1,2);//3
+            scoreBoard.Play(5,2);//7
+            scoreBoard.Play(1,2);//3
+
+            //then
+            Assert.AreEqual(91,scoreBoard.TotalScore);
+        }
+    }
+}
