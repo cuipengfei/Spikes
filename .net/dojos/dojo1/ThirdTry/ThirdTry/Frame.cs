0f5c7c8cb0fc0801faf0a0dd18eb4e818f37a40d
cuipengfei <cuipengfei2008@gmail.com>
2015-09-14 23:18:25 +0800

    first test case: calculate score without bonus

---
+++ .net/dojos/dojo1/ThirdTry/ThirdTry/Frame.cs
@@ -0,0 +1,21 @@
+﻿namespace ThirdTry
+{
+    public class Frame
+    {
+        public int FirstBall { get; }
+        public int SecondBall { get; }
+
+        public Frame(int firstBall, int secondBall)
+        {
+            this.FirstBall = firstBall;
+            this.SecondBall = secondBall;
+        }
+
+        public int Score => CalculateScore();
+
+        private int CalculateScore()
+        {
+            return FirstBall+SecondBall;
+        }
+    }
+}
---
+++ .net/dojos/dojo1/ThirdTry/ThirdTryTest/FrameTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThirdTry;
+
+namespace ThirdTryTest
+{
+    [TestClass]
+    public class FrameTest
+    {
+        [TestMethod]
+        public void FrameShouldCountItsOwnScoreWithoutBonus()
+        {
+            //given
+            var frame = new Frame(1,2);
+
+            //when
+            int score=frame.Score;
+
+            //then
+            Assert.AreEqual(3,score);
+        }
+    }
+}
