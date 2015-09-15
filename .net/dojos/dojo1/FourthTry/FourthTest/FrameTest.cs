540c7ca8a52f1c919add9da57938fcdd38193498
cuipengfei <cuipengfei2008@gmail.com>
2015-09-15 22:19:36 +0800

    step1: create project and calculate score when there is no bonus

---
+++ .net/dojos/dojo1/FourthTry/FourthTest/FrameTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FourthTry;
+
+namespace FourthTest
+{
+    [TestClass]
+    public class FrameTest
+    {
+        [TestMethod]
+        public void FrameShouldCountScoreWhenThereIsNoBonus()
+        {
+            //given
+            var frame = new Frame(2,3);
+
+            //when
+            int score=frame.Score;
+
+            //then
+            Assert.AreEqual(5,score);
+        }
+    }
+}
---
+++ .net/dojos/dojo1/FourthTry/FourthTry/Frame.cs
@@ -0,0 +1,16 @@
+﻿namespace FourthTry
+{
+    public class Frame
+    {
+        public int FirstBall { get; }
+        public int SecondBall { get; }
+
+        public Frame(int firstBall, int secondBall)
+        {
+            this.FirstBall = firstBall;
+            this.SecondBall = secondBall;
+        }
+
+        public int Score { get { return FirstBall + SecondBall; } }
+    }
+}
