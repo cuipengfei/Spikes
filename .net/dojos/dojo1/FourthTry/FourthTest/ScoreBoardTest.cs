04af306627a405ca96a8b898624c26629ae1dff8
cuipengfei <cuipengfei2008@gmail.com>
2015-09-15 22:48:47 +0800

    step 7: score board calculates total score of 10 frames

---
+++ .net/dojos/dojo1/FourthTry/FourthTest/ScoreBoardTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FourthTry;
+
+namespace FourthTest
+{
+    [TestClass]
+    public class ScoreBoardTest
+    {
+        [TestMethod]
+        public void ScoreBoardShouldCalculateTotalScoreOf10Frames()
+        {
+            //given
+            var scoreBoard=new ScoreBoard();
+
+            //when
+            scoreBoard.Play(1,2);//3
+            scoreBoard.Play(2,3);//5
+            scoreBoard.Play(3,4);//7
+            scoreBoard.Play(5,5);//11
+            scoreBoard.Play(1,2);//3
+            scoreBoard.Play(10,0);//20
+            scoreBoard.Play(7,3);//11
+            scoreBoard.Play(1,2);//3
+            scoreBoard.Play(10,0);//13
+            scoreBoard.Play(1,2);//3
+
+            int total = scoreBoard.TotalScore;
+            //then
+            Assert.AreEqual(79,total);
+        }
+    }
+}
---
+++ .net/dojos/dojo1/FourthTry/FourthTry/ScoreBoard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FourthTry
+{
+    public class ScoreBoard
+    {
+        private readonly List<Frame> frames=new List<Frame>(); 
+        public ScoreBoard()
+        {
+        }
+
+        public int TotalScore { get { return frames.Sum(frame => frame.Score); } }
+
+        public void Play(int firstBall, int secondBall)
+        {
+            var frame = new Frame(firstBall, secondBall);
+            if (frames.Count > 0)
+            {
+                frames.Last().Next = frame;
+            }
+            frames.Add(frame);
+        }
+    }
+}
