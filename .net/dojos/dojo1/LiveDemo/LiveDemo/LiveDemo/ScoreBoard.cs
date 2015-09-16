ce4555cd9b7eca01ab8dd38c0c3d64a1df258493
cuipengfei <cuipengfei2008@gmail.com>
2015-09-16 11:33:56 +0800

    step 8: total score of perfect game

--- .net/dojos/dojo1/LiveDemo/LiveDemo/LiveDemo/ScoreBoard.cs
+++ .net/dojos/dojo1/LiveDemo/LiveDemo/LiveDemo/ScoreBoard.cs
@@ -8,14 +8,24 @@ namespace LiveDemo
     {
         private List<Frame> frames=new List<Frame>(); 
 
-        public void Play(int firstBall, int secondBall)
+        public void Play(int firstBall, int secondBall,int thirdBall=-1)
         {
-            var frame=new Frame(firstBall,secondBall);
+            var frame = CreateFrame(firstBall, secondBall, thirdBall);
             if (frames.Count > 0)
             {
                 frames.Last().Next = frame;
             }
             frames.Add(frame);
+        }
+
+        private static Frame CreateFrame(int firstBall, int secondBall, int thirdBall)
+        {
+            var frame = new Frame(firstBall, secondBall);
+            if (thirdBall != -1)
+            {
+                frame = new LastFrame(firstBall, secondBall, thirdBall);
+            }
+            return frame;
         }
 
         public int TotalScore
--- .net/dojos/dojo1/LiveDemo/LiveDemo/LiveDemoTest/ScoreBoardTest.cs
+++ .net/dojos/dojo1/LiveDemo/LiveDemo/LiveDemoTest/ScoreBoardTest.cs
@@ -30,5 +30,49 @@ namespace LiveDemoTest
             //then
             Assert.AreEqual(75,scoreBoard.TotalScore);
         }
+        
+        [TestMethod]
+        public void ScoreBoardShouldCountTotalScoreOfWhenLastFrameHas3Balls()
+        {
+            //given
+            var scoreBoard=new ScoreBoard();
+
+            //when
+            scoreBoard.Play(1,2);//3
+            scoreBoard.Play(2,2);//4
+            scoreBoard.Play(3,2);//5
+            scoreBoard.Play(4,6);//10+1
+            scoreBoard.Play(1,2);//3
+            scoreBoard.Play(5,5);//10+1
+            scoreBoard.Play(1,9);//10+7
+            scoreBoard.Play(7,2);//9
+            scoreBoard.Play(3,6);//9
+            scoreBoard.Play(1,9,2);//12
+
+            //then
+            Assert.AreEqual(84,scoreBoard.TotalScore);
+        }
+
+        [TestMethod]
+        public void ScoreBoardShouldCountTotalScoreOfPerfectGame()
+        {
+            //given
+            var scoreBoard=new ScoreBoard();
+
+            //when
+            scoreBoard.Play(10,0);
+            scoreBoard.Play(10,0);
+            scoreBoard.Play(10,0);
+            scoreBoard.Play(10,0);
+            scoreBoard.Play(10,0);
+            scoreBoard.Play(10,0);
+            scoreBoard.Play(10,0);
+            scoreBoard.Play(10,0);
+            scoreBoard.Play(10,0);
+            scoreBoard.Play(10,10,10);
+
+            //then
+            Assert.AreEqual(300,scoreBoard.TotalScore);
+        }
     }
 }
