1ed6fc7466f0a5692246a9af629ead47bbaaaba7
cuipengfei <cuipengfei2008@gmail.com>
2015-09-20 14:45:19 +0800

    first try init

---
+++ .net/dojos/dojo2/FirstTry/GildedRose/GildedRoseTest/GildedRoseTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GildedRoseTest
+{
+    [TestClass]
+    public class GildedRoseTest
+    {
+        [TestMethod]
+        public void TestTheTruth()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
