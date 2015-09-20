71f32428058135865013af6736201494b95dca64
cuipengfei <cuipengfei2008@gmail.com>
2015-09-20 16:00:19 +0800

    back stage done

--- .net/dojos/dojo2/FirstTry/GildedRose/GildedRoseTest/BackStagePassTest.cs
+++ .net/dojos/dojo2/FirstTry/GildedRose/GildedRoseTest/BackStagePassTest.cs
@@ -23,7 +23,7 @@ namespace GildedRoseTest
         }
 
         [TestMethod]
-        public void BackStagePassShouldIncreaseBy2Before10DaysSellIn()
+        public void BackStagePassShouldIncreaseBy2After10DaysSellIn()
         {
             //given
             var program = GetProgram();
@@ -34,9 +34,51 @@ namespace GildedRoseTest
             //then 10 days left
             Assert.AreEqual(BackStagePass(program).SellIn,10);
 
-            //when
+            //when another 5 days
             PassNDays(program,5);
+
+            //then 10 more quality
             Assert.AreEqual(BackStagePass(program).Quality,35);
+        }
+
+        [TestMethod]
+        public void BackStagePassShouldIncreaseBy3After5DaysSellIn()
+        {
+            //given
+            var program = GetProgram();
+
+            //when pass 10 days
+            PassNDays(program,10);
+
+            //then 5 days left
+            Assert.AreEqual(5,BackStagePass(program).SellIn);
+
+            //when pass another 5 days
+            PassNDays(program,5);
+
+            //then quality is 50
+            Assert.AreEqual(50,BackStagePass(program).Quality);
+        }
+
+
+        [TestMethod]
+        public void BackStagePassShouldDropToZeroAfterSellInDaysZeroLeft()
+        {
+            //given
+            var program = GetProgram();
+
+            //when pass 15 days
+            PassNDays(program,15);
+
+            //then 0 days left
+            Assert.AreEqual(0,BackStagePass(program).SellIn);
+            Assert.AreEqual(50, BackStagePass(program).Quality);
+
+            //when pass another 1 day
+            PassNDays(program,1);
+
+            //then quality is 50
+            Assert.AreEqual(0, BackStagePass(program).Quality);
         }
     }
 }
