766af0ae96764c38a2a10ec790e494ccab798a4b
cuipengfei <cuipengfei2008@gmail.com>
2015-09-21 11:05:45 +0800

    built

---
+++ .net/FSCheckSpike/CSFSCheck/ClassesToTest/Counter.cs
@@ -0,0 +1,25 @@
+﻿namespace FsCheck.NUnit.CSharpExamples.ClassesToTest
+{
+    public class Counter
+    {
+        private int n;
+
+        public void Inc()
+        {
+            n++;
+        }
+
+        public void Dec()
+        {
+            if (n > 2)
+                n -= 2;
+            else
+                n--;
+        }
+
+        public int Get()
+        {
+            return n;
+        }
+    }
+}
---
+++ .net/FSCheckSpike/CSFSCheck/Examples.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using FsCheck.NUnit.CSharpExamples.ClassesToTest;
+using NUnit.Framework;
+
+namespace FsCheck.NUnit.CSharpExamples
+{
+    [TestFixture]
+    public class Examples
+    {
+        //Simple boolean property
+        [Property]
+        public bool RevRev(int[] xs)
+        {
+            return xs.Reverse().Reverse().SequenceEqual(xs);
+        }
+
+        // Note: should fail
+        [Property(Verbose = true)]
+        public Property RevId_shouldFail()
+        {
+            return Prop.ForAll<int[]>(xs => xs.Reverse().SequenceEqual(xs));
+        }
+
+        //TODO: do not call toProperty.
+        // Note this one should fail
+        [Property]
+        public Property Counter_shouldFail()
+        {
+            return new CounterSpec().ToProperty();
+        }
+    }
+}
