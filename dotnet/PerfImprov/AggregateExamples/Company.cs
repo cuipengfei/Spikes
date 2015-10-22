71d6cd8c0bcaf16db67cd5317d54a0e680adeb5c
cuipengfei <cuipengfei2008@gmail.com>
2015-10-22 14:42:50 +0800

    indentation

--- dotnet/PerfImprov/AggregateExamples/Company.cs
+++ dotnet/PerfImprov/AggregateExamples/Company.cs
@@ -7,10 +7,10 @@
         public Company Merge(Company that)
         {
             var justToWasteTime = EvaluatedMarketValue/100;
-            justToWasteTime  *= 100;
+            justToWasteTime *= 100;
             justToWasteTime += 100;
             justToWasteTime -= 100;
-           
+
             EvaluatedMarketValue += that.EvaluatedMarketValue;
             return this;
         }
