83ea758310ba9bf1b293fa30ca11df7eaf3f7b3d
cuipengfei <cuipengfei2008@gmail.com>
2015-11-05 14:47:47 +0800

    di

---
+++ dotnet/IntegrationTestSpike/AddDefenceTest/AddTowerTestWithIOC.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using IntegrationTestSpike.WithIOC;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Autofac;
+using IntegrationTestSpike.WithIOC.Steps;
+
+namespace AddDefenceTest
+{
+    [TestClass]
+    public class AddTowerTestWithIOC
+    {
+        [TestMethod]
+        public void ShouldAddTowerToDefencelessLands()
+        {
+            var step = Launcher.RegisterComponents().Resolve<CalculateStep>();
+
+            Assert.AreEqual(0, GlobalContext.Instance.ExistingLands[0].Towers.Count); //before: no tower
+            step.Do();
+            Assert.AreEqual(1, GlobalContext.Instance.ExistingLands[0].Towers.Count); //after: one tower
+            Assert.AreEqual("6", GlobalContext.Instance.ExistingLands[0].Towers[0].ID);
+        }
+    }
+}
