93513a3bb2b066c82f136fbc036ffe3bbffdc151
cuipengfei <cuipengfei2008@gmail.com>
2015-11-09 09:54:37 +0800

    new test

---
+++ dotnet/IntegrationTestSpike/AddDefenceTest/AddTowerTestWithoutIOC.cs
@@ -0,0 +1,29 @@
+﻿using IntegrationTestSpike.WithoutIOC;
+using IntegrationTestSpike.WithoutIOC.Providers;
+using IntegrationTestSpike.WithoutIOC.Steps;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddDefenceTest
+{
+    [TestClass]
+    public class AddTowerTestWithoutIOC
+    {
+        [TestMethod]
+        public void WholeProcessTest()
+        {
+            var prep = new PrepareStep(GlobalContext.Instance);
+            prep.Init(new ExcelDataProvider(), new DatFileDataProvider());
+
+            Assert.AreEqual(null, GlobalContext.Instance.ExistingLands);
+            prep.Do();
+            Assert.AreEqual(1, GlobalContext.Instance.ExistingLands.Count);
+
+            var calc = new CalculateStep(GlobalContext.Instance);
+
+            GlobalContext.Instance.MinimumDefence = 5;
+            Assert.AreEqual(0, GlobalContext.Instance.ExistingLands[0].Towers.Count);
+            calc.Do();
+            Assert.AreEqual(1, GlobalContext.Instance.ExistingLands[0].Towers.Count);
+        }
+    }
+}
