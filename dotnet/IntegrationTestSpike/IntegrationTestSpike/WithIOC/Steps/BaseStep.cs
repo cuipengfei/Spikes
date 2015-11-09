b24df8d9640edbf9aa30409491b5417afc9749ff
cuipengfei <cuipengfei2008@gmail.com>
2015-11-09 10:56:03 +0800

    format

--- dotnet/IntegrationTestSpike/IntegrationTestSpike/WithIOC/Steps/BaseStep.cs
+++ dotnet/IntegrationTestSpike/IntegrationTestSpike/WithIOC/Steps/BaseStep.cs
@@ -7,19 +7,14 @@ namespace IntegrationTestSpike.WithIOC.Steps
     public abstract class BaseStep
     {
         protected readonly GlobalContext _context;
-        public DataProvider<List<Land>> LandDataProvider { get; set; }
-        public DataProvider<List<Tower>> TowerDataProvider { get; set; }
 
         protected BaseStep(GlobalContext context)
         {
             _context = context;
         }
 
-//        public void Init(DataProvider<List<Land>> landDataProvider, DataProvider<List<Tower>> towerDataProvider)
-//        {
-//            this.LandDataProvider = landDataProvider;
-//            this.TowerDataProvider = towerDataProvider;
-//        }
+        public DataProvider<List<Land>> LandDataProvider { get; set; }
+        public DataProvider<List<Tower>> TowerDataProvider { get; set; }
 
         public abstract void Do();
     }
