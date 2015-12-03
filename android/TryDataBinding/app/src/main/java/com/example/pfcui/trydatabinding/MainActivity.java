125f045e9a1408b1d13c82ac54fff80d6e03888a
cuipengfei <cuipengfei2008@gmail.com>
2015-12-03 23:31:54 +0800

    simple model to view one way binding

--- android/TryDataBinding/app/src/main/java/com/example/pfcui/trydatabinding/MainActivity.java
+++ android/TryDataBinding/app/src/main/java/com/example/pfcui/trydatabinding/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.pfcui.trydatabinding;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.pfcui.trydatabinding.databinding.ActivityMainBinding;
+import com.example.pfcui.trydatabinding.models.User;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(
+                this, R.layout.activity_main);
+        binding.setUser(new User("this is data model", "set from activity"));
     }
 }
---
+++ android/TryDataBinding/app/src/main/java/com/example/pfcui/trydatabinding/models/User.java
@@ -0,0 +1,19 @@
+package com.example.pfcui.trydatabinding.models;
+
+public class User {
+    private final String firstName;
+    private final String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
