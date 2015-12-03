898be672acb52ec0c2868534e5bf7fc568d3b079
cuipengfei <cuipengfei2008@gmail.com>
2015-12-04 00:21:15 +0800

    simple model to view update

--- android/TryDataBinding/app/src/main/java/com/example/pfcui/trydatabinding/MainActivity.java
+++ android/TryDataBinding/app/src/main/java/com/example/pfcui/trydatabinding/MainActivity.java
@@ -3,17 +3,26 @@ package com.example.pfcui.trydatabinding;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.example.pfcui.trydatabinding.databinding.ActivityMainBinding;
 import com.example.pfcui.trydatabinding.models.User;
 
 public class MainActivity extends AppCompatActivity {
 
+    private User user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = DataBindingUtil.setContentView(
                 this, R.layout.activity_main);
-        binding.setUser(new User("this is data model", "set from activity"));
+        user = new User("this is data model", "set from activity");
+        binding.setUser(user);
+    }
+
+    public void changeName(View view) {
+        user.setFirstName("this is changed");
+        user.setLastName("this is also changed");
     }
 }
--- android/TryDataBinding/app/src/main/java/com/example/pfcui/trydatabinding/models/User.java
+++ android/TryDataBinding/app/src/main/java/com/example/pfcui/trydatabinding/models/User.java
@@ -1,19 +1,36 @@
 package com.example.pfcui.trydatabinding.models;
 
-public class User {
-    private final String firstName;
-    private final String lastName;
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+import com.example.pfcui.trydatabinding.BR;
+
+public class User extends BaseObservable {
+    private String firstName;
+    private String lastName;
 
     public User(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
+    @Bindable
     public String getFirstName() {
         return firstName;
     }
 
+    @Bindable
     public String getLastName() {
         return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+        notifyPropertyChanged(BR.firstName);
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+        notifyPropertyChanged(BR.lastName);
     }
 }
