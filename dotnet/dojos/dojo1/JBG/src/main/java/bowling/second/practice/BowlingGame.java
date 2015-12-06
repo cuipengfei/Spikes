c55aee31342b66543f9097f37ed50e2171ee17ec
cuipengfei <cuipengfei2008@gmail.com>
2015-12-06 13:01:11 +0800

    8. count total scores of 10 frames

---
+++ dotnet/dojos/dojo1/JBG/src/main/java/bowling/second/practice/BowlingGame.java
@@ -0,0 +1,31 @@
+package bowling.second.practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BowlingGame {
+    private List<Frame> frames = new ArrayList<>();
+
+    public void play(int firstBall, int secondBall) throws Exception {
+        Frame frame = new Frame(firstBall, secondBall);
+        connectFrames(frame);
+    }
+
+    public void play(int firstBall, int secondBall, int thirdBall) throws Exception {
+        Frame frame = new LastFrame(firstBall, secondBall, thirdBall);
+        connectFrames(frame);
+    }
+
+    private void connectFrames(Frame frame) throws Exception {
+        if (frames.size() > 0) {
+            frames.get(frames.size() - 1).setNextFrame(frame);
+        }
+        frames.add(frame);
+    }
+
+    public int totalScore() {
+        return frames.stream()
+                .map(Frame::countScore)
+                .reduce(0, (acc, next) -> acc + next);
+    }
+}
---
+++ dotnet/dojos/dojo1/JBG/src/test/java/bowling/second/practice/BowlingGameTest.java
@@ -0,0 +1,30 @@
+package bowling.second.practice;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class BowlingGameTest {
+    @Test
+    public void shouldCountScoreOf10Frames() throws Exception {
+        //given
+        BowlingGame game = new BowlingGame();
+
+        //when
+        game.play(2, 3); //5
+        game.play(2, 8); //12
+        game.play(2, 7); //9
+        game.play(3, 6); //9
+        game.play(10, 0); //20
+        game.play(5, 5); //20
+        game.play(10, 0); //30
+        game.play(10, 0); //22
+        game.play(10, 0); //20
+        game.play(2, 8, 5); //15
+
+        int total = game.totalScore();
+        //then
+        assertThat(total, is(162));
+    }
+}
