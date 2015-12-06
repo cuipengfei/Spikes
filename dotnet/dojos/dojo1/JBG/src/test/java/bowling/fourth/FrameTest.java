766ac847ff37d78dce9d5f214eeb2e715d0d907c
cuipengfei <cuipengfei2008@gmail.com>
2015-12-06 15:19:57 +0800

    4th - step3 - score with 2 strikes bonus when second strike is last

--- dotnet/dojos/dojo1/JBG/src/test/java/bowling/fourth/FrameTest.java
+++ dotnet/dojos/dojo1/JBG/src/test/java/bowling/fourth/FrameTest.java
@@ -61,4 +61,18 @@ public class FrameTest {
         //then
         assertThat(score, is(24));
     }
+
+    @Test
+    public void shouldCountScoreWith2StrikeBonusWhenSecondStrikeIsLastFrame() throws Exception {
+        //given
+        Frame frame1 = new Frame(10, 0);
+        Frame frame2 = new Frame(10, 0);
+        frame1.setNext(frame2);
+
+        //when
+        int score = frame1.countScore();
+
+        //then
+        assertThat(score, is(20));
+    }
 }
