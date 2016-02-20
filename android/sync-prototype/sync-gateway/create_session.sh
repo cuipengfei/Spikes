9ad88a7059f15795bfb77a6f7bf505cae0fc48e5
cuipengfei <cuipengfei2008@gmail.com>
2016-02-20 17:59:23 +0800

    script to create user and session

---
+++ android/sync-prototype/sync-gateway/create_session.sh
@@ -0,0 +1,1 @@
+curl -H 'Content-Type: application/json' -X POST -d '{"name":"user1"}' http://127.0.0.1:4985/demodb/_session
---
+++ android/sync-prototype/sync-gateway/create_user.sh
@@ -0,0 +1,1 @@
+curl -H 'Content-Type: application/json' -X POST -d '{"name":"user1", "password":"password1","all_channels":["*"],"admin_channels":["*"]}' http://127.0.0.1:4985/demodb/_user/
