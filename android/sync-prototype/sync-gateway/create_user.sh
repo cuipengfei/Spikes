37c58ba6fca0fc309d18bb9a698a301d3935b28c
cuipengfei <cuipengfei2008@gmail.com>
2016-02-22 00:42:16 +0800

    assign channel to user

--- android/sync-prototype/sync-gateway/create_user.sh
+++ android/sync-prototype/sync-gateway/create_user.sh
@@ -1,1 +1,1 @@
-curl -H 'Content-Type: application/json' -X POST -d '{"name":"user1", "password":"password1","all_channels":["*"],"admin_channels":["*"]}' http://127.0.0.1:4985/demodb/_user/
+curl -H 'Content-Type: application/json' -X POST -d '{"name":"user1", "password":"password1","all_channels":["user1"],"admin_channels":["user1"]}' http://127.0.0.1:4985/demodb/_user/
