c30e0a50d044795cb0963e5d5b62c0ecc4f7249f
cuipengfei <cuipengfei2008@gmail.com>
2016-02-28 16:47:24 +0800

    test name was wrong

--- kata/cash-register/src/test/java/cashregister/models/viewmodels/PlainTextViewModelTest.java
+++ kata/cash-register/src/test/java/cashregister/models/viewmodels/PlainTextViewModelTest.java
@@ -42,6 +42,24 @@ public class PlainTextViewModelTest {
     public void shouldOutPutFinalSummary() throws Exception {
         PlainTextViewModel plainTextViewModel = new PlainTextViewModel();
 
+        plainTextViewModel.addToOriginalTotal(10d);
+        plainTextViewModel.addToDiscountedTotal(9d);
+
+        plainTextViewModel.addToOriginalTotal(10d);
+        plainTextViewModel.addToDiscountedTotal(9d);
+
+        String linesSectionInString = plainTextViewModel.toString();
+        assertThat(linesSectionInString, is("***<没钱赚商店>购物清单***\n" +
+                "----------------------\n" +
+                "总计: 18.00(元)\n" +
+                "节省：2.00(元)\n" +
+                "**********************"));
+    }
+
+    @Test
+    public void plainTextOutputIntegrationTest() throws Exception {
+        PlainTextViewModel plainTextViewModel = new PlainTextViewModel();
+
         plainTextViewModel.addToLinesSection("apple");
         plainTextViewModel.addToOriginalTotal(10d);
         plainTextViewModel.addToDiscountedTotal(9d);
@@ -60,24 +78,6 @@ public class PlainTextViewModelTest {
                 "something discount\n" +
                 "apple something\n" +
                 "orange something\n" +
-                "----------------------\n" +
-                "总计: 18.00(元)\n" +
-                "节省：2.00(元)\n" +
-                "**********************"));
-    }
-
-    @Test
-    public void plainTextOutputIntegrationTest() throws Exception {
-        PlainTextViewModel plainTextViewModel = new PlainTextViewModel();
-
-        plainTextViewModel.addToOriginalTotal(10d);
-        plainTextViewModel.addToDiscountedTotal(9d);
-
-        plainTextViewModel.addToOriginalTotal(10d);
-        plainTextViewModel.addToDiscountedTotal(9d);
-
-        String linesSectionInString = plainTextViewModel.toString();
-        assertThat(linesSectionInString, is("***<没钱赚商店>购物清单***\n" +
                 "----------------------\n" +
                 "总计: 18.00(元)\n" +
                 "节省：2.00(元)\n" +
