513a75b43ff38ece10b015be139cdd056975c095
cuipengfei <cuipengfei2008@gmail.com>
2016-02-28 15:38:48 +0800

    summary section

--- kata/cash-register/src/main/java/cashregister/models/viewmodels/PlainTextViewModel.java
+++ kata/cash-register/src/main/java/cashregister/models/viewmodels/PlainTextViewModel.java
@@ -1,26 +1,59 @@
 package cashregister.models.viewmodels;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class PlainTextViewModel {
-    private static String newLine = System.getProperty("line.separator");
-    private List<String> linesSection = new ArrayList<>();
+    private static final String newLine = System.getProperty("line.separator");
+    private static final String sectionSeparator = "----------------------";
 
-    public void addToLines(String line) {
+    private List<String> linesSection = new ArrayList<>();
+    private HashMap<String, List<String>> sections = new HashMap<>();
+
+    public void addToLinesSection(String line) {
         linesSection.add(line);
+    }
+
+    public void createSection(String sectionName) {
+        sections.put(sectionName, new ArrayList<>());
+    }
+
+    public void addToSection(String sectionName, String line) {
+        sections.get(sectionName).add(line);
     }
 
     public String output() {
         StringBuilder stringBuilder = new StringBuilder("***<没钱赚商店>购物清单***");
+        outputLinesSection(stringBuilder);
+        outputCustomSections(stringBuilder);
+        stringBuilder
+                .append(newLine)
+                .append("**********************");
+        return stringBuilder.toString();
+    }
+
+    private void outputCustomSections(StringBuilder stringBuilder) {
+        for (String sectionName : sections.keySet()) {
+            List<String> section = sections.get(sectionName);
+            if (section.size() > 0) {
+                stringBuilder.append(newLine);
+                stringBuilder.append(sectionName);
+                for (String line : section) {
+                    stringBuilder.append(newLine);
+                    stringBuilder.append(line);
+                }
+                stringBuilder.append(newLine).append(sectionSeparator);
+            }
+        }
+    }
+
+    private void outputLinesSection(StringBuilder stringBuilder) {
         for (String line : linesSection) {
             stringBuilder
                     .append(newLine)
                     .append(line);
         }
-        stringBuilder
-                .append(newLine)
-                .append("**********************");
-        return stringBuilder.toString();
+        stringBuilder.append(newLine).append(sectionSeparator);
     }
 }
--- kata/cash-register/src/test/java/cashregister/models/viewmodels/PlainTextViewModelTest.java
+++ kata/cash-register/src/test/java/cashregister/models/viewmodels/PlainTextViewModelTest.java
@@ -10,11 +10,28 @@ public class PlainTextViewModelTest {
     public void shouldOutPutLinesSection() throws Exception {
         PlainTextViewModel plainTextViewModel = new PlainTextViewModel();
 
-        plainTextViewModel.addToLines("what ever line");
+        plainTextViewModel.addToLinesSection("what ever line");
 
         String linesSectionInString = plainTextViewModel.output();
         assertThat(linesSectionInString, is("***<没钱赚商店>购物清单***\n" +
                 "what ever line\n" +
+                "----------------------\n" +
+                "**********************"));
+    }
+
+    @Test
+    public void shouldOutPutDiscountSection() throws Exception {
+        PlainTextViewModel plainTextViewModel = new PlainTextViewModel();
+
+        plainTextViewModel.createSection("whatever discount");
+        plainTextViewModel.addToSection("whatever discount", "blah blah");
+
+        String linesSectionInString = plainTextViewModel.output();
+        assertThat(linesSectionInString, is("***<没钱赚商店>购物清单***\n" +
+                "----------------------\n" +
+                "whatever discount\n" +
+                "blah blah\n" +
+                "----------------------\n" +
                 "**********************"));
     }
 }
