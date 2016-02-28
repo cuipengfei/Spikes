9de5ea86791c4548dafe27489c03fb85aef9a7b9
cuipengfei <cuipengfei2008@gmail.com>
2016-02-28 15:51:10 +0800

    integration test for view model

--- kata/cash-register/src/test/java/cashregister/models/viewmodels/PlainTextViewModelTest.java
+++ kata/cash-register/src/test/java/cashregister/models/viewmodels/PlainTextViewModelTest.java
@@ -40,7 +40,7 @@ public class PlainTextViewModelTest {
     }
 
     @Test
-    public void shouldOutPutFinalSummary() throws Exception {
+    public void plainTextOutputIntegrationTest() throws Exception {
         PlainTextViewModel plainTextViewModel = new PlainTextViewModel();
 
         plainTextViewModel.addToOriginalTotal(10d);
@@ -51,6 +51,35 @@ public class PlainTextViewModelTest {
 
         String linesSectionInString = plainTextViewModel.output();
         assertThat(linesSectionInString, is("***<没钱赚商店>购物清单***\n" +
+                "----------------------\n" +
+                "总计: 18.00(元)\n" +
+                "节省：2.00(元)\n" +
+                "**********************"));
+    }
+
+    @Test
+    public void shouldOutPutFinalSummary() throws Exception {
+        PlainTextViewModel plainTextViewModel = new PlainTextViewModel();
+
+        plainTextViewModel.addToLinesSection("apple");
+        plainTextViewModel.addToOriginalTotal(10d);
+        plainTextViewModel.addToDiscountedTotal(9d);
+        plainTextViewModel.createSection("something discount");
+        plainTextViewModel.addToSection("something discount", "apple something");
+
+        plainTextViewModel.addToLinesSection("orange");
+        plainTextViewModel.addToOriginalTotal(10d);
+        plainTextViewModel.addToDiscountedTotal(9d);
+        plainTextViewModel.addToSection("something discount", "orange something");
+
+        String linesSectionInString = plainTextViewModel.output();
+        assertThat(linesSectionInString, is("***<没钱赚商店>购物清单***\n" +
+                "apple\n" +
+                "orange\n" +
+                "----------------------\n" +
+                "something discount\n" +
+                "apple something\n" +
+                "orange something\n" +
                 "----------------------\n" +
                 "总计: 18.00(元)\n" +
                 "节省：2.00(元)\n" +
