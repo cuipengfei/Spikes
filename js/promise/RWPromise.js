a36b9b4270f4b3384c7dac7efc0ac2d3ba88cb06
cuipengfei <cuipengfei2008@gmail.com>
2016-04-28 15:23:23 +0800

    允许调用者把promise置于fulfilled或者是rejected的状态,29个测试通过

--- js/promise/RWPromise.js
+++ js/promise/RWPromise.js
@@ -1,11 +1,32 @@
+function RWPromise() {
+    var self = this;
+    self.isFrozen = false;
+
+    self.then = function (onFulfilled, onRejected) {
+        self.onFulfilled = onFulfilled;
+        self.onRejected = onRejected;
+    };
+
+    self.resolve = function (value) {
+        if (!self.isFrozen) {
+            self.onFulfilled(value);
+            self.isFrozen = true;
+        }
+    };
+
+    self.reject = function (reason) {
+        if (!self.isFrozen) {
+            self.onRejected(reason);
+            self.isFrozen = true;
+        }
+    };
+}
+
 module.exports.deferred = function () {
-    console.log("hello, deferred is running");
-};
-
-module.exports.resolved = function (value) {
-    console.log("hello, resolved is running");
-};
-
-module.exports.rejected = function (reason) {
-    console.log("hello, rejected is running");
+    var rwPromise = new RWPromise();
+    return {
+        promise: rwPromise,
+        resolve: rwPromise.resolve,
+        reject: rwPromise.reject
+    };
 };
