daec8b2d2f7c82eb724c340d684e480bab3c6223
cuipengfei <cuipengfei2008@gmail.com>
2016-04-28 16:09:00 +0800

    pass 33 tests

--- js/promise/RWPromise.js
+++ js/promise/RWPromise.js
@@ -1,21 +1,23 @@
 function RWPromise() {
     var self = this;
     self.isFrozen = false;
+    self.isInitiated = false;
 
     self.then = function (onFulfilled, onRejected) {
         self.onFulfilled = onFulfilled;
         self.onRejected = onRejected;
+        self.isInitiated = true;
     };
 
     self.resolve = function (value) {
-        if (!self.isFrozen) {
+        if (self.isInitiated && !self.isFrozen) {
             self.onFulfilled(value);
             self.isFrozen = true;
         }
     };
 
     self.reject = function (reason) {
-        if (!self.isFrozen) {
+        if (self.isInitiated && !self.isFrozen) {
             self.onRejected(reason);
             self.isFrozen = true;
         }
