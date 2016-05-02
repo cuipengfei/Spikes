57367f2938e55bb87c1769e9a257eef4bc1468cb
cuipengfei <cuipengfei2008@gmail.com>
2016-05-02 15:02:18 +0800

    consolidate state strings

--- js/promise/RWPromise.js
+++ js/promise/RWPromise.js
@@ -1,93 +1,87 @@
 var n = 0;
+
 function RWPromise() {
+    var states = {pending: "pending", resolved: "resolved", rejected: "rejected"};
+
     var self = this;
     self.callBacks = [];
-    self.branches = [];
-    self.name = n++;
+    self.children = [];
+    self.state = states.pending;
+    self.name = n++;// this is only for debugging, the name is like: 1 -> 1's son -> 1's grandson
 
     self.resolve = function (value) {
-        if (!isSettled(self)) {
-            self.x = value;
-            kickOffChainReaction(true);
-        }
+        self.tryResolveWith(value, states.resolved);
         return self;
     };
 
     self.reject = function (reason) {
-        if (!isSettled(self)) {
-            self.x = reason;
-            kickOffChainReaction(false);
-        }
+        self.tryResolveWith(reason, states.rejected);
         return self;
     };
 
-    self.then = function (onFulfilled, onRejected) {
-        var branch = new RWPromise();
-        branch.name = self.name + " - " + branch.name;
-        branch.callBacks.push({onFulFilled: onFulfilled, onRejected: onRejected});
+    self.tryResolveWith = function (x, state) {
 
-        self.branches.push(branch);
-
-        setTimeout(function () {
-            if (isSettled(self)) {
-                if (self.state === "resolved") {
-                    branch.resolve(self.x);
-                } else if (self.state === "rejected") {
-                    branch.reject(self.x);
-                }
-            }
-        });
-
-        return branch;
-    };
-
-    var isSettled = function (p) {
-        return p.state === "resolved" || p.state === "rejected";
-    };
-
-    var kickOffChainReaction = function (tryFulfill) {
-        setTimeout(function () {
+        function resolveSelf() {
             if (self.callBacks.length === 0) {
-                if (tryFulfill) {
-                    self.state = "resolved";
-                } else {
-                    self.state = "rejected";
-                }
+                self.state = state;
             }
             else {
                 self.callBacks.forEach(function (callBack) {
+                    var isResolved = self.state === states.resolved;
+                    var isTryingToResolve = (self.state === states.pending && state === states.resolved);
+                    var f = (isResolved || isTryingToResolve) ? callBack.onFulFilled : callBack.onRejected;
 
-                    var f = (self.state === "resolved" || (self.state === undefined && tryFulfill)) ? callBack.onFulFilled : callBack.onRejected;
                     if (typeof f === "function") {
                         try {
                             self.x = f(self.x);
-                            self.state = "resolved";
+                            self.state = states.resolved;
                         } catch (err) {
                             self.x = err;
-                            self.state = "rejected";
+                            self.state = states.rejected;
                         }
                     } else {
-                        if (self.state === undefined) {
-                            if (tryFulfill) {
-                                self.state = "resolved";
-                            } else {
-                                self.state = "rejected";
-                            }
+                        if (self.state === states.pending) {
+                            self.state = state;
                         }
                     }
                 });
             }
+        }
 
-            if (self.state === "resolved") {
-                self.branches.forEach(function (branch) {
-                    branch.resolve(self.x);
-                });
-            } else {
-                self.branches.forEach(function (branch) {
-                    branch.reject(self.x);
-                });
+        function resolveChildren() {
+            self.children.forEach(function (branch) {
+                branch.tryResolveWith(self.x, self.state);
+            });
+        }
+
+        if (!isSettled()) {
+            self.x = x;
+
+            setTimeout(function () {
+                resolveSelf();
+                resolveChildren();
+            });
+        }
+    };
+
+    self.then = function (onFulfilled, onRejected) {
+        var child = new RWPromise();
+        child.name = self.name + " -> " + child.name;
+        child.callBacks.push({onFulFilled: onFulfilled, onRejected: onRejected});
+
+        self.children.push(child);
+
+        setTimeout(function () {
+            if (isSettled(self)) {
+                child.tryResolveWith(self.x, self.state);
             }
         });
+
+        return child;
+    };
+
+    var isSettled = function () {
+        return self.state === states.resolved || self.state === states.rejected;
     };
 }
 
