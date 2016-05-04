820a1b5157c2520c9ae829ca3d132e8ae45d7106
cuipengfei <cuipengfei2008@gmail.com>
2016-05-04 22:47:12 +0800

    even shorter

--- js/promise/Promise3.js
+++ js/promise/Promise3.js
@@ -1,7 +1,7 @@
 function Promise() {
-    var self = this
-    self.callbacks = []
-    self.status = 'pending'
+    var self = this;
+    self.callbacks = [];
+    self.status = 'pending';
 
     self.resolve = function (value) {
         if (value instanceof Promise) {
@@ -11,8 +11,8 @@ function Promise() {
             if (self.status !== 'pending') {
                 return
             }
-            self.status = 'resolved'
-            self.data = value
+            self.status = 'resolved';
+            self.data = value;
 
             for (var i = 0; i < self.callbacks.length; i++) {
                 self.callbacks[i].onResolved(value)
@@ -25,11 +25,11 @@ function Promise() {
             if (self.status !== 'pending') {
                 return
             }
-            self.status = 'rejected'
-            self.data = reason
+            self.status = 'rejected';
+            self.data = reason;
 
             for (var i = 0; i < self.callbacks.length; i++) {
-                self.callbacks[i].onRejected(reason)
+                self.callbacks[i].onRejected(reason);
             }
         })
     };
@@ -37,71 +37,54 @@ function Promise() {
     self.then = function (onResolved, onRejected) {
         onResolved = typeof onResolved === 'function' ? onResolved : function (v) {
             return v
-        }
+        };
         onRejected = typeof onRejected === 'function' ? onRejected : function (r) {
             throw r
-        }
+        };
         var promise2 = new Promise();
-
-        if (self.status === 'resolved') {
-            setTimeout(function () {
-                try {
-                    resolvePromise(promise2, onResolved(self.data))
-                } catch (e) {
-                    return promise2.reject(e)
-                }
-            })
-        }
-
-        if (self.status === 'rejected') {
-            setTimeout(function () {
-                try {
-                    resolvePromise(promise2, onRejected(self.data))
-                } catch (e) {
-                    return promise2.reject(e)
-                }
-            })
-        }
 
         if (self.status === 'pending') {
             self.callbacks.push({
                 onResolved: function (value) {
                     try {
-                        resolvePromise(promise2, onResolved(value))
+                        resolutionProcedure(promise2, onResolved(value));
                     } catch (e) {
-                        return promise2.reject(e)
+                        return promise2.reject(e);
                     }
                 },
                 onRejected: function (reason) {
                     try {
-                        resolvePromise(promise2, onRejected(reason))
+                        resolutionProcedure(promise2, onRejected(reason));
                     } catch (e) {
-                        return promise2.reject(e)
+                        return promise2.reject(e);
                     }
+                }
+            })
+        } else {
+            setTimeout(function () {
+                try {
+                    var x;
+                    if (self.status === 'resolved') {
+                        x = onResolved(self.data);
+                    } else if (self.status === 'rejected') {
+                        x = onRejected(self.data);
+                    }
+                    resolutionProcedure(promise2, x);
+                } catch (e) {
+                    return promise2.reject(e);
                 }
             })
         }
 
-        return promise2
+        return promise2;
     };
 }
 
-function resolvePromise(promise, x) {
+function resolutionProcedure(promise, x) {
     var then
     var thenCalledOrThrow = false
     if (promise === x) {
         return promise.reject(new TypeError('Chaining cycle detected for promise!'))
-    }
-
-    if (x instanceof Promise) {
-        if (x.status === 'pending') {
-            x.then(function (v) {
-                resolvePromise(promise, v);
-            }, promise.reject);
-        } else {
-            x.then(promise.resolve, promise.reject)
-        }
-        return
     }
 
     if ((x !== null) && ((typeof x === 'object') || (typeof x === 'function'))) {
@@ -111,7 +94,7 @@ function resolvePromise(promise, x) {
                 then.call(x, function rs(y) {
                     if (thenCalledOrThrow) return
                     thenCalledOrThrow = true
-                    return resolvePromise(promise, y)
+                    return resolutionProcedure(promise, y)
                 }, function rj(r) {
                     if (thenCalledOrThrow) return
                     thenCalledOrThrow = true
@@ -137,4 +120,4 @@ module.exports.deferred = function () {
         resolve: promise.resolve,
         reject: promise.reject
     }
-}
+};
