06de5d5e291933986659ef527c8f2dbe4154ebc4
cuipengfei <cuipengfei2008@gmail.com>
2016-05-09 17:04:54 +0800

    starter kit for  the first question

--- js/promise/workshop_plan/workshop1/test/workshop1_test.js
+++ js/promise/workshop_plan/workshop1/test/workshop1_test.js
@@ -7,6 +7,8 @@ var expect = chai.expect;
 var chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 
+var nock = require('nock');
+
 describe('sanity check', function () {
     it('if you can see this test pass, it means your env is set up properly', function () {
         assert.equal(workshop1.one, 1);
@@ -16,6 +18,15 @@ describe('sanity check', function () {
 
 describe('get git hub repos', function () {
     it('should pass an array of 23 repo names to onFullfilled', function () {
-        return expect(workshop1.getMyGithubRepos()).to.eventually.have.length(23);//I have 23 repos
+        //given: mock http response
+        nock('https://api.github.com')
+            .get('/users/cuipengfei/repos')
+            .reply(200, [{name: 'a'}, {name: 'b'}]);
+
+        //when
+        var reposPromise = workshop1.getMyGithubRepos();
+
+        //then
+        return expect(reposPromise).to.eventually.deep.equal(['a', 'b']);
     });
 });
