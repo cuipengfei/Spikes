757c520f546b21b4e15f01d70d90eef7f76f3258
cuipengfei <cuipengfei2008@gmail.com>
2016-05-09 16:35:42 +0800

    starter kit for  the first question

--- js/promise/workshop_plan/workshop1/test/workshop1_test.js
+++ js/promise/workshop_plan/workshop1/test/workshop1_test.js
@@ -1,9 +1,21 @@
-var assert = require('chai').assert;
 var workshop1 = require("../workshop1");
+
+var chai = require('chai');
+var assert = chai.assert;
+var expect = chai.expect;
+
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
 
 describe('sanity check', function () {
     it('if you can see this test pass, it means your env is set up properly', function () {
         assert.equal(workshop1.one, 1);
         assert.notEqual(workshop1.one, 2);
+    });
+});
+
+describe('get git hub repo', function () {
+    it('gets an array of github repo names under my account', function () {
+        return expect(workshop1.getMyGithubRepos()).to.eventually.have.length(23);//I have 23 repos
     });
 });
--- js/promise/workshop_plan/workshop1/workshop1.js
+++ js/promise/workshop_plan/workshop1/workshop1.js
@@ -3,28 +3,31 @@
     var Promise = require("bluebird");
 
     module.exports.getMyGithubRepos = function () {
-        var responseBody = "";
-        https.get({
-                host: 'api.github.com',
-                path: '/users/cuipengfei/repos',
-                headers: {
-                    'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13'
-                }
-            }, function (res) {
-                console.log('Got response: ' + res.statusCode);
-                res.on('data', function (chunk) {
-                    responseBody += chunk;
-                });
-                res.on('end', function () {
-                    console.log(
-                        JSON.parse(responseBody).map(function (repo) {
+        return new Promise(function (resolve, reject) {
+            var responseBody = "";
+            https.get({
+                    host: 'api.github.com',
+                    path: '/users/cuipengfei/repos',
+                    headers: {
+                        'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13'
+                    }
+                }, function (res) {
+                    console.log('Got response: ' + res.statusCode);
+                    res.on('data', function (chunk) {
+                        responseBody += chunk;
+                    });
+                    res.on('end', function () {
+                        var repoNames = JSON.parse(responseBody).map(function (repo) {
                             return repo.name;
-                        }));
+                        });
+
+                        console.log(repoNames);
+                    });
                 })
-            })
-            .on('error', function (e) {
-                console.log('Got error: ' + e.message);
-            });
+                .on('error', function (e) {
+                    console.log('Got error: ' + e.message);
+                });
+        });
     };
 
     module.exports.one = 1;
