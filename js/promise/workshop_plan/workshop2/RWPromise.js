096f1ac27faaf3c4526c69f02fd8785c28b5df0d
cuipengfei <cuipengfei2008@gmail.com>
2016-05-10 16:28:51 +0800

    min work to make 2.2.1 work

--- js/promise/workshop_plan/workshop2/RWPromise.js
+++ js/promise/workshop_plan/workshop2/RWPromise.js
@@ -1,15 +1,63 @@
 function RWPromise() {
 
     var self = this;
+    var childrenResolutions = [];
+
+    var isSettled = function () {
+        return self.state === 'resolved' || self.state === 'rejected';
+    };
+
+    var changeStateTo = function (state, x) {
+        if (!isSettled()) {
+            self.x = x;
+            self.state = state;
+            childrenResolutions.forEach(function (childResolution) {
+                childResolution(x);
+            });
+        }
+    };
 
     self.resolve = function (value) {
+        changeStateTo('resolved', value);
     };
 
     self.reject = function (reason) {
+        changeStateTo('rejected', reason);
     };
 
     self.then = function (onFulfilled, onRejected) {
+        var child = new RWPromise();
 
+        function childResolution() {
+            try {
+                onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : function (v) {
+                    return v;
+                };
+
+                onRejected = typeof onRejected === 'function' ? onRejected : function (r) {
+                    throw r;
+                };
+
+                var x;
+                if (self.state === 'resolved') {
+                    x = onFulfilled(self.x);
+                    child.resolve(x);
+                } else {
+                    x = onRejected(self.x);
+                    child.reject(x);
+                }
+            } catch (err) {
+                child.reject(x);
+            }
+        }
+
+        if (isSettled()) {
+            childResolution();
+        } else {
+            childrenResolutions.push(childResolution);
+        }
+
+        return child;
     };
 }
 
