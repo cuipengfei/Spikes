49f7fbaa400e356a6345873fc42d9ad51aacf2e0
cuipengfei <cuipengfei2008@gmail.com>
2016-05-31 14:32:05 +0800

    use post json body to insert users

--- PgPlusNoSql/src/main/java/hello/pgexample/controllers/HomeController.java
+++ PgPlusNoSql/src/main/java/hello/pgexample/controllers/HomeController.java
@@ -2,13 +2,15 @@ package hello.pgexample.controllers;
 
 import hello.pgexample.domain.AppUser;
 import hello.pgexample.repository.UserRepository;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 public class HomeController {
@@ -16,17 +18,17 @@ public class HomeController {
     @Inject
     UserRepository userRepository;
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = GET)
     public String sayHello() {
         return "Hello there !";
     }
 
-    @RequestMapping(value = "/user/{username}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public AppUser create(@PathVariable String username) {
-        return userRepository.save(new AppUser(username));
+    @RequestMapping(value = "/user", method = POST, consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    public AppUser create(@RequestBody AppUser user) {
+        return userRepository.save(user);
     }
 
-    @RequestMapping(value = "/user", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/user", method = GET, produces = APPLICATION_JSON_VALUE)
     public Iterable<AppUser> findAll() {
         return userRepository.findAll();
     }
