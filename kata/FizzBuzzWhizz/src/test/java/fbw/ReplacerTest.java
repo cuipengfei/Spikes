5e4852b2587776e70795a979b7b82ce65bcf9c7b
pfcui <cuipengfei2008@gmail.com>
2016-10-12 13:23:53 +0800

    not call next if previous matches

--- kata/FizzBuzzWhizz/src/test/java/fbw/ReplacerTest.java
+++ kata/FizzBuzzWhizz/src/test/java/fbw/ReplacerTest.java
@@ -5,6 +5,7 @@ import org.mockito.Mockito;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
 public class ReplacerTest {
@@ -25,7 +26,7 @@ public class ReplacerTest {
     }
 
     @Test
-    public void should_connect_replacers_in_a_chain_and_call_in_order() throws Exception {
+    public void should_call_replacers_in_a_chain_in_order() throws Exception {
         //given
         Replacer replacer1 = Mockito.spy(StubReplacer.class);
         Replacer replacer2 = Mockito.spy(StubReplacer.class);
@@ -42,5 +43,25 @@ public class ReplacerTest {
         Mockito.verify(replacer1).tryReplace(123);
         Mockito.verify(replacer2).tryReplace(123);
         Mockito.verify(replacer3).tryReplace(123);
+    }
+
+    @Test
+    public void should_not_call_next_replacer_when_previous_matches() throws Exception {
+        //given
+        Replacer replacer1 = Mockito.spy(StubReplacer.class);
+        Replacer replacer2 = Mockito.spy(StubReplacer.class);
+        Replacer replacer3 = Mockito.spy(StubReplacer.class);
+
+        when(replacer2.tryReplace(123)).thenReturn("some word");
+
+        //when
+        replacer1.chain(replacer2).chain(replacer3);
+        String word = replacer1.replace(123);
+
+        //then
+        assertThat(word, is("some word"));
+        Mockito.verify(replacer1).tryReplace(123);
+        Mockito.verify(replacer2).tryReplace(123);
+        Mockito.verify(replacer3, times(0)).tryReplace(123);
     }
 }
