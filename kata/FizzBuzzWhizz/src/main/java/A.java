272713ce42283878c3d170313d3bad905cf68391
pfcui <cuipengfei2008@gmail.com>
2016-10-12 11:55:02 +0800

    sanity check

---
+++ kata/FizzBuzzWhizz/src/main/java/A.java
@@ -0,0 +1,5 @@
+/**
+ * Created by pfcui on 10/12/16.
+ */
+public class A {
+}
---
+++ kata/FizzBuzzWhizz/src/test/java/ATest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by pfcui on 10/12/16.
+ */
+public class ATest {
+    @Test
+    public void sanity() throws Exception {
+        assertEquals(1, 2);
+    }
+}
