a0f43142464b8c2a6666d285d9e26b7ef7e5a9a0
pfcui <cuipengfei2008@gmail.com>
2017-03-01 00:38:55 +0800

    2. frame count bonus score of spare

--- dotnet/dojos/dojo1/JBG/src/main/java/bowling2017/practice2/Frame.java
+++ dotnet/dojos/dojo1/JBG/src/main/java/bowling2017/practice2/Frame.java
@@ -3,13 +3,26 @@ package bowling2017.practice2;
 public class Frame {
   private final int roll1;
   private final int roll2;
+  private int ownScore;
+  private final boolean isSpare;
+
+  private Frame next;
 
   public Frame(int roll1, int roll2) {
     this.roll1 = roll1;
     this.roll2 = roll2;
+    ownScore = roll1 + roll2;
+    isSpare = roll1 + roll2 == 10;
+  }
+
+  public void setNext(Frame next) {
+    this.next = next;
   }
 
   public int countScore() {
-    return roll1 + roll2;
+    if (isSpare) {
+      return ownScore + next.roll1;
+    }
+    return ownScore;
   }
 }
--- dotnet/dojos/dojo1/JBG/src/test/java/bowling2017/practice2/FrameTest.java
+++ dotnet/dojos/dojo1/JBG/src/test/java/bowling2017/practice2/FrameTest.java
@@ -12,4 +12,15 @@ public class FrameTest {
     int score = frame.countScore();
     assertThat(score, is(3));
   }
+
+  @Test
+  public void frameShouldCountBonusForSpare() throws Exception {
+    Frame frame = new Frame(2, 8);
+    Frame frame2 = new Frame(3, 5);
+    frame.setNext(frame2);
+
+    int score = frame.countScore();
+
+    assertThat(score, is(13));
+  }
 }
