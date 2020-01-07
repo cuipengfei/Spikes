2f7e23cac1c074c766b692415ea9ca6526970fc8
cuipengfei <cuipengfei2008@gmail.com>
2020-01-07 18:55:28 +0800

    blog code

---
+++ scala/edx-reactive-course/Assignment-2-BinTree/src/main/scala/TraitSelfTypeBlog/JNotUser.java
@@ -0,0 +1,7 @@
+package TraitSelfTypeBlog;
+
+public class JNotUser implements Tweeter {
+    private String abc() {
+        return "abc";
+    }
+}
---
+++ scala/edx-reactive-course/Assignment-2-BinTree/src/main/scala/TraitSelfTypeBlog/SelfTypeBlog.scala
@@ -0,0 +1,23 @@
+package TraitSelfTypeBlog
+
+trait User {
+  def username: String
+}
+
+trait Tweeter {
+  self: User => // reassign this
+  def tweet(tweetText: String) = println(s"$username: $tweetText")
+}
+
+class VerifiedTweeter(val username_ : String)  {
+  def username = s"real $username_"
+}
+
+object SelfTypeBlog {
+  def main(args: Array[String]): Unit = {
+    val realBeyoncé = new VerifiedTweeter("Beyoncé") with User with Tweeter
+    realBeyoncé.tweet("Just spilled my glass of lemonade")
+
+    new JNotUser().tweet("hello")
+  }
+}
