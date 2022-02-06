8339c3f8b2df25afe191181d612624f4dc7281b2
cuipengfei <cuipengfei2008@gmail.com>
2022-02-06 21:08:24 +0800

    py basics

---
+++ python/script1.py
@@ -0,0 +1,32 @@
+print("hello, ni hao")
+
+x = 1
+if x == 1:
+    print("x is one")
+
+myfloat = 7.2
+print(myfloat)
+myfloat = float("7.2")
+print(myfloat)
+
+a, b = 3, 4
+print(a, b)
+
+for x in [1, 2, 3, "hello"]:
+    print(x)
+
+squared = 7 ** 2
+cubed = 2 ** 3
+print(squared)
+print(cubed)
+
+lotsofhellos = "重复 " * 10
+print(lotsofhellos)
+
+print(["列表"]+["可以拼"])
+
+print(["列表", "可以重复"]*2)
+
+print("Hello, %s!" % "abc")
+
+print("%s is %d years old." % ("xiao ming", 12345))
