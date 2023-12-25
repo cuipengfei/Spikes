aad6d8540c315e6f55418b7dd374050c8b824e1d
cuipengfei <cuipengfei2008@gmail.com>
2023-12-25 17:31:30 +0800

    test parallel stream

--- jpa/lock-transaction-threads/src/main/java/com/github/spring/example/TestParallelStreamThreads.java
+++ jpa/lock-transaction-threads/src/main/java/com/github/spring/example/TestParallelStreamThreads.java
@@ -4,19 +4,37 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
 import java.util.stream.IntStream;
 
-public class TestPS {
+public class TestParallelStreamThreads {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
+        System.out.println("pool");
+        withPool();
+
+        System.out.println("----------");
+
+        System.out.println("no pool");
+        noPool();
+    }
+
+    private static void withPool() throws InterruptedException, ExecutionException {
         System.out.println("outside " + Thread.currentThread().getName());
 
         ForkJoinPool customThreadPool = new ForkJoinPool(4);
         customThreadPool.submit(() -> {
             System.out.println("middle " + Thread.currentThread().getName());
 
-            IntStream.range(1, 100).parallel().forEach(num -> {
+            IntStream.range(1, 10).parallel().forEach(num -> {
                 System.out.println("inside " + Thread.currentThread().getName());
             });
         }).get();
 
         customThreadPool.shutdown();
+    }
+
+    private static void noPool() {
+        System.out.println("outside " + Thread.currentThread().getName());
+
+        IntStream.range(1, 10).parallel().forEach(num -> {
+            System.out.println("inside " + Thread.currentThread().getName());
+        });
     }
 }
