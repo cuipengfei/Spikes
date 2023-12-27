dca02bb8e7a20de52169363c96621116702ee665
cuipengfei <cuipengfei2008@gmail.com>
2023-12-27 17:48:50 +0800

    not always use jdbc

--- jpa/lock-transaction-threads/src/main/java/com/github/spring/example/service/Problem2BadFixService.java
+++ jpa/lock-transaction-threads/src/main/java/com/github/spring/example/service/Problem2BadFixService.java
@@ -3,7 +3,6 @@ package com.github.spring.example.service;
 import com.github.spring.example.CatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.integration.jdbc.lock.JdbcLockRegistry;
 import org.springframework.integration.support.locks.LockRegistry;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
--- jpa/lock-transaction-threads/src/main/java/com/github/spring/example/service/Problem2GoodFixService.java
+++ jpa/lock-transaction-threads/src/main/java/com/github/spring/example/service/Problem2GoodFixService.java
@@ -2,7 +2,6 @@ package com.github.spring.example.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.integration.jdbc.lock.JdbcLockRegistry;
 import org.springframework.integration.support.locks.LockRegistry;
 import org.springframework.stereotype.Service;
 
